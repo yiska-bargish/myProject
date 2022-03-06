5547d5e55d69cc468d9f59764e1b20e250a089dd
Isca <Isca>
2022-03-06 21:19:25 +0200

    New

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -16,6 +16,7 @@ export default function Home() {
 
     return (
         <>
+        <p>ggg</p>
             {/* <img className="nisayon" src={image} ></img> */}
             <img className="nisayon" src={newimg} ></img>
             <div style={{ backgroundImage: i, width: '500px' }}></div>
