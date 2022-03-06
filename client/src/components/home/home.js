7b6184315791fc2c00c59f1df36a612163ca52a7
ayalay062 <ayalay062@gmail.com>
2022-03-06 22:32:10 +0200

    Ayala

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -6,17 +6,56 @@ import { IconButton, Icon } from '@mui/material'
 import i from '../../assets/img/14.jpg'
 // import image from './image.jpg'
 import newimg from './25.jpg'
+import newimg2 from './new.jpg'
+import newimg3 from './image.jpg'
 import { Link } from 'react-router-dom'
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 export default function Home() {
 
     const dispatch = useDispatch()
     const data = useSelector(state => state)
     const categories = useSelector(state => state.product.categories)
 
+    const createCarouselItemImage = (index, options = {}) => (
+        <div key={index}>
+            <img src={`/assets/${index}.jpeg`} />
+            {/* <p className="legend">Legend {index}</p> */}
+        </div>
+    );
+    
+    const baseChildren = <div>{[1, 2, 3, 4, 5].map(createCarouselItemImage)}</div>;
+    
+    const tooglesGroupId = 'Toggles';
+    const valuesGroupId = 'Values';
+    const mainGroupId = 'Main';
+    
+    const getConfigurableProps = () => ({
+        showArrows: false,
+        showStatus: false,
+        showIndicators: true,
+        infiniteLoop: true,
+        showThumbs: false,
+        useKeyboardArrows: true,
+        autoPlay: true,
+        stopOnHover: true,
+        swipeable: true,
+        dynamicHeight: true,
+        emulateTouch:true,
+        autoFocus:false,
+        thumbWidth: 100,
+        selectedItem: 0,
+        interval: 2000,
+        transitionTime: 500,
+        swipeScrollTolerance: 5,
+        ariaLabel: '',
+    });
+    
+
     return (
         <>
             {/* <img className="nisayon" src={image} ></img> */}
+            <Carousel {...getConfigurableProps()}>{baseChildren.props.children}</Carousel>
             <img className="nisayon" src={newimg} ></img>
             <div style={{ backgroundImage: i, width: '500px' }}></div>
             <div className='home'>
